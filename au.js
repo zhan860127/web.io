7ad3eae70e4f1a10831023be3cd3057e2559a897
zhan860127 <61983044+zhan860127@users.noreply.github.com>
2020-04-08 20:11:32 +0800

    Add files via upload

---
+++ au.js
@@ -0,0 +1,64 @@
+var snum=0;
+var num=0;
+var i=0;
+$(document).ready(function(){
+    $(".himg").mouseenter(function(){
+       $(this).animate({opacity:"1"},1000);
+    });
+    $(".himg").mouseleave(function(){
+        $(this).animate({opacity:"0"},1000);
+      });
+ 
+      $(".himg").bind('click',function(e){
+          if(snum!=0)
+          {
+            $('#i'+snum).fadeOut('slow');
+            snum=0;
+          }
+           num= parseInt($(this).attr('id').substr(1));
+          snum=25-num;
+          console.log('#i'+snum);
+        $('#i'+snum).fadeIn('slow');
+        event.stopImmediatePropagation();
+        
+      });
+     /* $(".himg").tap(function(){
+        if(snum!=0)
+        {
+          $('#i'+snum).fadeOut('slow');
+          snum=0;
+        }
+        var num= parseInt($(this).attr('id').substr(1));
+        snum=25-num;
+        console.log('#i'+snum);
+      $('#i'+snum).fadeIn('slow');
+    });*/
+
+  });
+  $(document).bind('click',function(e) {
+    var target = e.target;
+
+    if (!$(target).is('#i'+snum)&&snum!=0 &&snum!=1 ) {
+        $('#i'+snum).fadeOut('slow');
+        snum=0;
+        event.stopImmediatePropagation();
+    }
+});
+if(i==0)
+{
+$(function(){ $('#i19').click(function(){ 
+    $('html,body').animate({scrollTop:$('#t').offset().top}, 500);});  
+}); 
+i++;
+}
+/*$(document).tap(function(e) {
+    var target = e.target;
+
+    if (!$(target).is('#i'+snum)&&snum!=0 ) {
+        $('#i'+snum).fadeOut('slow');
+        snum=0;
+    }
+});*/
+  
+
+  
